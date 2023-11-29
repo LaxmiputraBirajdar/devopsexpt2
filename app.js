f86b062751e81168f629b231d5682b6dcabd4002
LaxmiputraBirajdar <laxmiputrabirajdar1515@gmail.com>
2023-11-29 23:41:34 +0530

    added

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+
+// Import the express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route to handle HTTP GET requests
+app.get('/', (req, res) => {
+  // Send an HTML response
+  res.send('<h1>Hello, Node.js!</h1>');
+});
+
+// Start the server on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
